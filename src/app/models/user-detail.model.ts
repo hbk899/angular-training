3861699d39276f2f764351ef1e6e2e06f3ac33b3
hbk899 <bkhalid.bscs16seecs@seecs.edu.pk>
2023-08-12 21:07:41 +0000

    ..

--- src/app/models/user-detail.model.ts
+++
@@ -1,9 +0,0 @@
-export class UserDetails {
-    name: string = ""
-    age: number = 0
-    background?: string = "#ffffff"
-}
-export class Todo {
-    assignedTo: string = ""
-    name: string = ""
-}
